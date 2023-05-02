03a4bd3dddde0d470d864e2e6d9fc92b9e2e343e
Jung Daeun <94734426+jde98@users.noreply.github.com>
2023-05-02 18:57:33 +0900

    초기세팅
    백 초기세팅

---
+++ board/src/test/java/com/de/board/BoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.de.board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
